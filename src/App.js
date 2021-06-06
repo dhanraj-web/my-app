ee9563538a2199c0cdbc24f027a35c369a7e9368
dhanraj <dhanraj@aequalisys.com>
2021-06-06 16:56:31 +0530

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TableContainer from "./components/TableContainer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TableContainer />
     </div>
   );
 }
---
+++ src/components/TableContainer.js
@@ -0,0 +1,12 @@
+import "./../App.css";
+import TableData from "./TableData";
+
+function TableContainer() {
+  return (
+    <div className="tab__container">
+      <TableData />
+    </div>
+  );
+}
+
+export default TableContainer;
